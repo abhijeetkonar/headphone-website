3210023f8ca06ece52aac89782a7fbd9847182a0
abhijeetkonar <113918917+abhijeetkonar@users.noreply.github.com>
2023-07-20 21:15:59 +0530

    uploaded the code & images file for this webpage

---
+++ index.js
@@ -0,0 +1,19 @@
+gsap.from(".h-right img", {
+    x: 800,
+    y: -1000,
+    duration: 0.8,
+    delay: 0.5,
+})
+
+gsap.from(".shadow", {
+    opacity: 0,
+    duration: 1,
+    delay: 1.5,
+})
+
+gsap.from(".h-left", {
+    y: -50,
+    duration: 0.5,
+    delay:0.5,
+    opacity: 0,
+})
